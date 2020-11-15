4b0799219e28f2598ba95dc4505b4fdcf181ac52
Kishore Newton <kishorenke@gmail.com>
2020-11-16 00:25:21 +0530

    Revert "check"
    This reverts commit e7281cd892d6db76dd22537eab3b5f571376e654.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { User } from '../models/user';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 charcters')
   ],
-  async (req: Request, res: Response) => {
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,10 +23,11 @@ router.post(
 
     const { email, password } = req.body;
 
-    const user = User.build({ email, password });
-    await user.save();
+    throw new DatabaseConnectionError();
 
-    res.status(201).send(user)
+    console.log('Creating a user');
+
+    res.send({});
   }
 );
 
