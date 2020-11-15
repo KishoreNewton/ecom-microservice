ac9e4f7780407d47c303e173d92fcd678364f34d
Kishore Newton <kishorenke@gmail.com>
2020-11-15 21:02:24 +0530

    fix: fixing the async request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -14,20 +14,29 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 charcters')
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-
     const { email, password } = req.body;
 
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    console.log('Creating a user');
+    console.log(existingUser)
 
-    res.send({});
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    } 
+    const user = User.build({
+      email,
+      password
+    });
+    await user.save();
+    res.status(201).send(user);
+  
   }
 );
 
