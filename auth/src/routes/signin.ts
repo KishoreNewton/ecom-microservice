d7bdbd9d0530b6d246b876f79703189cdc5a7a2b
Kishore Newton <kishorenke@gmail.com>
2020-11-17 08:26:20 +0530

    feat: validate request

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,17 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req: Request, res: Response) => {
-  res.send('Hi There!');
-});
+router.post(
+  '/api/users/signin',
+  validateRequest,
+  [body('email').isEmail().withMessage('E-Mail must be vaiid')],
+  body('password').trim().notEmpty().withMessage('You must supply a password'),
+  (req: Request, res: Response, next: NextFunction) => {
+    
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
 
@@ -16,14 +16,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 charcters')
   ],
+  validateRequest,
   async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      // throw new NotFoundError(); // Using sync + required library npm install express-async-errors
-      return next(new RequestValidationError(errors.array())); // Using async
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
